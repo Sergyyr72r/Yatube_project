c6f6ababf678556c27c0b2248b0131c0eda5c254
Romanov Sergey <romanovsa@list.ru>
2022-04-11 21:11:52 +0700

    Templats work fine

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,21 @@
+from turtle import title
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Main Page')
+    template = 'posts/index.html'
+    title = 'Это главная страница проекта Yatube'
+    context = {
+        'title' : title
+    }
+    return render(request, template, context) 
 
 def group_posts(request, slug):
-    return HttpResponse('Posts list')
+    template = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title' : title
+    }
+    return render(request, template, context) 
